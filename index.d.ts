0779d8cc2135cc5940a98ccc391653df8a9af8b0
jacobleesinger <jacobleesinger@gmail.com>
2021-02-24 18:56:16 -0800

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { CredentialRequest, NoPresentation, Presentation, PresentationRequestPostDto } from '@unumid/types';
+
+interface DemoBaseEntity {
+  uuid: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+type WithKeyAndValue<T, K extends string, V> = T & Record<K, V>;
+
+export type DemoDto<T, N extends string> = WithKeyAndValue<DemoBaseEntity, N, T>;
+export type WithVerification<T> = WithKeyAndValue<T, 'isVerified', boolean>;
+
+export type DemoSession = DemoBaseEntity;
+
+export interface DemoPresentationRequestOptions {
+  credentialRequests: CredentialRequest[];
+}
+
+export type DemoPresentationRequestDto = DemoDto<PresentationRequestPostDto, 'presentationRequest'>;
+
+export type DemoPresentationDto = WithVerification<DemoDto<Presentation, 'presentation'>>;
+
+export type DemoNoPresentationDto = WithVerification<DemoDto<NoPresentation, 'noPresentation'>>;
