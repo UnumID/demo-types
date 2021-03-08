f00a3756f319be3019266e5ab4c7548925912a66
jacobleesinger <jacobleesinger@gmail.com>
2021-03-08 09:37:42 -0800

    add comments

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,58 @@
-import { CredentialRequest, NoPresentation, Presentation, PresentationRequestPostDto } from '@unumid/types';
+import {
+  CredentialRequest,
+  NoPresentation,
+  Presentation,
+  PresentationRequestPostDto
+} from '@unumid/types';
 
+// base type which encapsulates properties shared by all database entities
+// in the demos
 interface DemoBaseEntity {
   uuid: string;
   createdAt: Date;
   updatedAt: Date;
 }
 
+// helper which adds a named key with a specific value type to an existing type
 type WithKeyAndValue<T, K extends string, V> = T & Record<K, V>;
 
+// base type which encapsulates properties shared by many objects returned
+// from the demo issuer and verifier apis
 export type DemoDto<T, N extends string> = WithKeyAndValue<DemoBaseEntity, N, T>;
+
+// helper type which adds a boolean 'isVerified' property to an existing type
 export type WithVerification<T> = WithKeyAndValue<T, 'isVerified', boolean>;
 
+// type of the Session entity used by demo verifiers
+// really just an alias for DemoBaseEntity, as Session entities
+// don't have any other meaningful properties
 export type DemoSession = DemoBaseEntity;
 
-export interface DemoPresentationRequestOptions {
+// type of the object expected by verifier servers to create a PresentationRequest
+export interface DemoPresentationRequestCreateOptions {
   credentialRequests: CredentialRequest[];
   metadata: { sessionUuid: string };
 }
 
+// type of the object returned by verifier servers when a PresentationRequest is created or gotten
 export type DemoPresentationRequestDto = DemoDto<PresentationRequestPostDto, 'presentationRequestPostDto'>;
 
+// type of the object published by verifier servers when a shared Presentation is received + verified
 export type DemoPresentationDto = WithVerification<DemoDto<Presentation, 'presentation'>>;
 
+// type of the object published by verifier servers when a shared NoPresentation is received + verified
 export type DemoNoPresentationDto = WithVerification<DemoDto<NoPresentation, 'noPresentation'>>;
 
+// type of the object expected by the issuer server to create a User
 export interface DemoUserCreateOptions {
   email: string;
   password: string;
   phone?: string;
 }
 
+// User type used by issuer server
 type DemoUser = DemoUserCreateOptions & DemoBaseEntity & { did?: string };
 
+// User type with the password field ommitted
+// returned by issuer api /user endpoint; shared between all demo applications
 export type DemoUserWithoutPassword = Omit<DemoUser, 'password'>;
