6e4d27b7145f46707016898c7b9341e1fd9465c4
jacobleesinger <jacobleesinger@gmail.com>
2021-03-16 10:27:18 -0700

    [DemoFcmRegistrationToken] rename Device -> FcmRegistrationToken

--- index.d.ts
+++ index.d.ts
@@ -43,9 +43,9 @@ export type DemoPresentationDto = WithVerification<DemoDto<Presentation, 'presen
 // type of the object published by verifier servers when a shared NoPresentation is received + verified
 export type DemoNoPresentationDto = WithVerification<DemoDto<NoPresentation, 'noPresentation'>>;
 
-// type of the Device object returned with DemoUsers
-export interface DemoDevice extends DemoBaseEntity {
-  fcmRegistrationToken: string;
+// type of the FcmRegistrationToken object returned with DemoUsers
+export interface DemoFcmRegistrationToken extends DemoBaseEntity {
+  token: string;
 }
 
 // type of the object expected by the issuer server to create a User
@@ -58,5 +58,5 @@ export interface DemoUserCreateOptions {
 // serialization of the User type used by issuer server
 type DemoUser = Omit<DemoUserCreateOptions, 'password'> & DemoBaseEntity & {
   did?: string;
-  devices: DemoDevice[];
+  fcmRegistrationTokens: DemoFcmRegistrationToken[];
 };
